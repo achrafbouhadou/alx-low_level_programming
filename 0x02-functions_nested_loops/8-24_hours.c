1f3b8a727f04de14affade0083a5abf13dcabe2a
achraf bouhadou <87522540+achrafbouhadou@users.noreply.github.com>
2022-06-08 00:43:43 +0200

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,11 +15,11 @@ void jack_bauer(void)
 		j = 0;
 		while (j < 60)
 		{
-			_putchar(i / 10);
-			_putchar(i % 10);
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar(j / 10);
-			_putchar(j % 10);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 			j++;
 		}
