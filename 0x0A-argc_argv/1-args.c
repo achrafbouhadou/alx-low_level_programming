eea97c74995b6b3390a5ebb21349c089c57b0c97
achraf bouhadou <87522540+achrafbouhadou@users.noreply.github.com>
2022-06-24 22:34:14 +0200

    #

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
   * @argv array of argument
   * Return: Always 0 (Success)
   */
-int main(int argc char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	print("%d\n", argc);
 	return (0);
