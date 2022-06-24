1b319ded7ddd612cfbab7d0fbe90e8a58326c028
achraf bouhadou <87522540+achrafbouhadou@users.noreply.github.com>
2022-06-24 22:46:57 +0200

    #

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
-  * -main prints the number of arguments 
-  * @argc number of argument
-  * @argv array of argument
-  * Return: Always 0 (Success)
-  */
+ * main - print the number of arguments passed to the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	print("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
