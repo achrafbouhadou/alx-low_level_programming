e87bc10b92d40136e0b51b553ca62368a598426d
achrafbouhadou <achraf1323@gmail.com>
2022-06-24 13:31:30 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * -main prints the number of arguments 
+  * @argc number of argument
+  * @argv array of argument
+  * Return: Always 0 (Success)
+  */
+int main(int argc char *argv[])
+{
+	print("%d\n", argc);
+	return (0);
+}
