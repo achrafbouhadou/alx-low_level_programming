66691eb53bfb6eb4e7694fdb8acbc5cce0fc7092
achrafbouhadou <achraf1323@gmail.com>
2022-07-01 15:27:07 -0700

    new project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <strio.h>
+#include <stdio.h>
 
 /*
  * main -print the phrase with proper grammar, 
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
- int main(void)
- {
+int main(void)
+{
 	 printf("with proper grammar, but the outcome is a piece of art,\n");
 	 return (0);
- }
+}
